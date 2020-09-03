c1de1adbe5367101cceff6cef3d26632f7c1eac1
Vinícius Ramos Kawamoto <vinirk@gmail.com>
2020-09-03 16:28:26 -0300

    Update and rename test.js to webflow-script.js

---
+++ webflow-script.js
@@ -0,0 +1,36 @@
+(function() {
+console.log("SCRIPT LOADED");
+function GetURLParameter(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++){
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam){
+            return sParameterName[1];
+        }
+    }
+}
+
+function callbackFunction(xmlhttp) 
+{
+    if (xmlhttp.response) {
+        console.log(xmlhttp.response);
+        localStorage.setItem("ad_cart", JSON.stringify({
+            "data":{
+                "data": JSON.parse(xmlhttp.response)
+            }
+        }));
+    }
+}
+
+var ml = GetURLParameter('ml');
+if(ml){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
+    xmlhttp.open("POST", "/cart/update.js", false);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    // xmlhttp.setRequestHeader('Authorization', 'Basic ' + window.btoa("magic" + ":" + "links")); 
+    xmlhttp.send(JSON.stringify({"attributes[ml]": ml}));
+    xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
+}
+}());
